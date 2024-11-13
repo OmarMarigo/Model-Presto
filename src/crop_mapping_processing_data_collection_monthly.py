155ddcc905effcb44e96960b0848151157fcd1b9
Clement <clementlonde23@gmail.com>
2024-11-13 11:38:53 +0000

    Authentification valid

--- src/crop_mapping_processing_data_collection_monthly.py
+++ src/crop_mapping_processing_data_collection_monthly.py
@@ -26,7 +26,7 @@ from google.cloud import storage
 from datetime import datetime, timedelta
  
 
-ee.Authenticate()
+
 ee.Initialize()
 
 """CLASSES_CODES={'mil': 10, 'mais': 11, 'arachide': 12, 'oseille': 13, 'sorgho': 14, 'niebe': 15,
